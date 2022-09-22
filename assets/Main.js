4d3aa0e028e5a88a5d8c3901532d790f1de24027
Zeno Driesen <zeno.driesen@gmail.com>
2022-09-22 15:19:19 +0200

    installed dependencies requiring the webpack encore bundle installed yarn installed react dependencies, including @babel, react router, axios also installed sweetalert2

---
+++ assets/Main.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ListEmployee from "./pages/ListEmployee"
+import AddEmployee from "./pages/AddEmployee"
+import EditEmployee from "./pages/EditEmployee"
+import ViewEmployee from "./pages/ViewEmployee"
+
+function Main() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<ListEmployee/>}/>
+                <Route path="/addEmployee" element={<AddEmployee/>}/>
+                <Route path="/editEmployee/:id" element={<EditEmployee/>}/>
+                <Route path="/showEmployee" element={<ViewEmployee/>}/>
+            </Routes>
+        </Router>
+    );
+}
+
+export default Main;
+if (document.getElementById('app')) {
+    const rootElement = document.getElementById("app");
+    const root = createRoot(rootElement);
+    root.render(
+        <StrictMode>
+            <Main/>
+        </StrictMode>
+    );
+}
