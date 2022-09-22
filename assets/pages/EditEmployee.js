b9cb27c26a009ba2fe7436a3b9f62623b72968d8
Zeno Driesen <zeno.driesen@gmail.com>
2022-09-22 16:01:11 +0200

    starting on the edit form

---
+++ assets/pages/EditEmployee.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from "react-router-dom";
+import Layout from "../components/Layout";
+import Swal from "sweetalert2";
+import axios from "axios";
+
+function EditEmployee() {
+
+}
